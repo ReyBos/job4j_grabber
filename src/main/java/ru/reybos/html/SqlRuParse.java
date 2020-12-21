f57c95f56f4e2be8103f52ca9a066f12ba6543e8
reybos <andreybosiy@yandex.ru>
2020-12-21 23:08:15 +0300

    2. Парсинг HTML страницы. [#404071]

---
+++ src/main/java/ru/reybos/html/SqlRuParse.java
@@ -0,0 +1,21 @@
+package ru.reybos.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements dateRow = doc.select("td[style].altCol");
+        for (int i = 0; i < row.size(); i++) {
+            Element href = row.get(i).child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element date = dateRow.get(i);
+            System.out.println(date.text());
+        }
+    }
+}
