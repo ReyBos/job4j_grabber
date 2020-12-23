3be5b4f4951d676d074e413dc67fd6fb3bfd4e06
reybos <andreybosiy@yandex.ru>
2020-12-24 00:15:04 +0300

    2.2. Модель данных - Post. [#404076]

--- src/main/java/ru/reybos/grabber/Post.java
+++ src/main/java/ru/reybos/grabber/Post.java
@@ -3,19 +3,15 @@ package ru.reybos.grabber;
 import java.util.Calendar;
 
 public class Post {
+    private String link;
+    private String title;
     private Calendar date;
     private String description;
 
-    public Post(Calendar date, String description) {
+    public Post(String link, String title, Calendar date, String description) {
+        this.link = link;
+        this.title = title;
         this.date = date;
         this.description = description;
-    }
-
-    public Calendar getDate() {
-        return date;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
