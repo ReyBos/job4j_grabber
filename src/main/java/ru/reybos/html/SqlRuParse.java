a3d02d2cfc4df66f0b4e21dba016ffb839e2f446
reybos <andreybosiy@yandex.ru>
2020-12-23 23:29:20 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#404075]

--- src/main/java/ru/reybos/html/SqlRuParse.java
+++ src/main/java/ru/reybos/html/SqlRuParse.java
@@ -5,26 +5,31 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 public class SqlRuParse {
     public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy, HH:mm");
     public static DateParser parser = new DateParser();
 
-    public static void main(String[] args) throws Exception {
-        for (int j = 1; j < 6; j++) {
-            String url = String.format("https://www.sql.ru/forum/job-offers/%d", j);
-            Document doc = Jsoup.connect(url).get();
-            Elements row = doc.select(".postslisttopic");
-            Elements dateRow = doc.select("td[style].altCol");
-            for (int i = 0; i < row.size(); i++) {
-                Element href = row.get(i).child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                Element date = dateRow.get(i);
-                System.out.println(date.text());
-                System.out.println(dateFormat.format(parser.parseDate(date.text()).getTime()));
-            }
+    public static void main(String[] args) throws IOException {
+        for (int i = 1; i <= 5; i++) {
+            parsePage(i);
+        }
+    }
+
+    public static void parsePage(int page) throws IOException {
+        String url = String.format("https://www.sql.ru/forum/job-offers/%d", page);
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".postslisttopic");
+        Elements dateRow = doc.select("td[style].altCol");
+        for (int i = 0; i < row.size(); i++) {
+            Element href = row.get(i).child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element date = dateRow.get(i);
+            System.out.println(date.text());
+            System.out.println(dateFormat.format(parser.parseDate(date.text()).getTime()));
         }
     }
 }
