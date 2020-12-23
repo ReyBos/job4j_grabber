a33a4481dc55f6ba010453c083ce7440ec9fec51
reybos <andreybosiy@yandex.ru>
2020-12-23 23:25:52 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#404075]

--- src/main/java/ru/reybos/html/SqlRuParse.java
+++ src/main/java/ru/reybos/html/SqlRuParse.java
@@ -12,16 +12,19 @@ public class SqlRuParse {
     public static DateParser parser = new DateParser();
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dateRow = doc.select("td[style].altCol");
-        for (int i = 0; i < row.size(); i++) {
-            Element href = row.get(i).child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element date = dateRow.get(i);
-            System.out.println(date.text());
-            System.out.println(dateFormat.format(parser.parseDate(date.text()).getTime()));
+        for (int j = 1; j < 6; j++) {
+            String url = String.format("https://www.sql.ru/forum/job-offers/%d", j);
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dateRow = doc.select("td[style].altCol");
+            for (int i = 0; i < row.size(); i++) {
+                Element href = row.get(i).child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element date = dateRow.get(i);
+                System.out.println(date.text());
+                System.out.println(dateFormat.format(parser.parseDate(date.text()).getTime()));
+            }
         }
     }
 }
